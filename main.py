97944884822c256a8dac43dba736e7a56bda293e
Ruofan1023 <69947227+Ruofan1023@users.noreply.github.com>
2022-01-04 23:53:01 +0800

    clean up

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import constants as keys
 from telegram.ext import *
 import zenQuote as zenQuote
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('I am Quote Bot')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 def start_command(update, context):
     update.message.reply_text("Type something interesting!")
 
